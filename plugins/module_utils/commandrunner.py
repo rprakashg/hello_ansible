f2578aa93efd06667579b1c18abcf18ba548fde6
Ram Gopinathan <rprakashg@gmail.com>
2024-07-30 09:16:13 -0700

    testing with module_utils

--- plugins/module_utils/commandrunner.py
+++ plugins/module_utils/commandrunner.py
@@ -10,7 +10,7 @@ class CommandRunner(object):
     def __init__(self, binary) -> None:
         self.binary = binary
 
-    def run(self, args) -> CommandResult:
+    def run(self, args: list) -> CommandResult:
         """
         Run command with subcommand and args in an executable binary
 
